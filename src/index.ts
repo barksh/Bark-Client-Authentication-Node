b27086fd2e4256f4104c26f7e0ed6d1e423788e3
WMXPY <wm@mengw.io>
2023-03-06 23:13:45 -0800

    release 1.2.0

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,12 @@
  */
 
 export * from "./action/v1/inquiry";
+export * from "./action/v1/public-key";
 export * from "./action/v1/redeem";
 export * from "./action/v1/refresh";
+export * from "./network/dns/cname";
 export * from "./proxy/v1/post-inquiry";
+export * from "./proxy/v1/post-public-key-fetch";
 export * from "./proxy/v1/post-redeem";
 export * from "./proxy/v1/post-refresh";
 
